c3a2f21e6781253f473b859a00d75c4e147550dd
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-06-06 18:00:35 +0300

    if else

---
+++ 02. if/02. if/Source.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	/*int a = 0, b = 0;
+	cout << "Enter first number " << endl;
+	cin >> a;
+	cout << "Enter second number " << endl;
+	cin >> b;
+
+	if (a > b) {
+		cout << "a > b" << endl;
+	}
+	else if (a < b) {
+		cout << "a < b" << endl;
+	}
+	else if (a < b) {
+		cout << "a < b" << endl;
+	}
+	else if (a < b) {
+		cout << "a < b" << endl;
+	}
+	else if (a < b) {
+		cout << "a < b" << endl;
+	}
+	else {
+		cout << "a = b" << endl;
+	}*/
+
+	/*char name_1[] = "Tom";
+	char name_2[] = "Tom";
+	cout << "Your name is " << name_1 << endl;
+	cout << "Your name is " << name_2 << endl;
+
+	if ("Tom" == "Tom") {
+		cout << "True!" << endl;
+	}
+	else {
+		cout << "False" << endl;
+	}*/
+
+	// Дано 3 числа.Визначити чи всі числа непарні.Числа вводяться з клавіатури
+
+	/*int a = 0;
+	cout << "Enter a " << endl;
+	cin >> a;
+
+
+	if (a % 2 == 0) {
+		cout << "a parne" << endl;
+	}
+	else if (a % 2 != 0) {
+		cout << "a ne parne" << endl;
+	}*/
+
+	// Написати програму яка в залежності від введеної години виводить: good night, good day, good evening,
+	// good morning.
+
+
+	//int time = 0;
+	//cout << "Enter time" << endl;
+	//cin >> time;
+
+	//if (time >= 0 && time < 6) {
+	//	cout << "Night " << time << endl;
+	//}
+	//else {
+	//	cout << "False" << endl;
+	//}
+
+	// 3. Розробити програму, що переводить значення температури в градусах по Цельсію в температуру 
+	// по Фаренгейту та навпаки. Співвідношення між температурами визначається формулою:
+	// TF = TC *1.8 +32. Діалог с користувачем реалізувати через систему меню.
+
+	/*int choice = 0;
+	cout << "1. C => F\n2. F => C" << endl;
+	cin >> choice;
+	cout << "Chocie =  " << choice << endl;
+	if (choice == 1) {
+
+	}*/
+	return 0;
+}
